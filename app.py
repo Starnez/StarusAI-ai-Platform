380ff23a5e3693caf787111737f9a86ef4654a93
Stokley <stokleyarnez@gmail.com>
2023-09-05 22:09:37 -0700

    added Test Login Screen

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import streamlit as st
 
+
 def main():
     st.title("Starus AI Content Creation Platform")
-    st.write("Welcome to the Starus AI content creation platform!")
-    st.write("More features will be added soon.")
+
+    # Display background image
+    st.image("path_to_your_image.jpg", use_column_width=True)
+
+    # Login button
+    if st.button("Login"):
+        show_login_form()
+
+
+def show_login_form():
+    st.subheader("Login to your account")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Submit"):
+        # Here, you'll integrate with Planetscale for authentication
+        # For now, let's just check if the fields are filled
+        if username and password:
+            st.success("Logged in successfully!")
+        else:
+            st.warning("Please enter your username and password.")
+
 
 if __name__ == "__main__":
     main()
