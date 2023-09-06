f41f077b7a9a7fe0c9c10c9eb5d98db460fb4690
Stokley <stokleyarnez@gmail.com>
2023-09-05 21:23:13 -0700

    added Streamlit app

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("Starus AI Content Creation Platform")
+    st.write("Welcome to the Starus AI content creation platform!")
+    st.write("More features will be added soon.")
+
+if __name__ == "__main__":
+    main()
