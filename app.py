b1fe9e08369cbf92c131b342e0502c4b1b700045
Stokley <stokleyarnez@gmail.com>
2023-09-05 23:25:16 -0700

    Fixes for Test Login Screen

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 import streamlit as st
 
+
 def main():
     st.title("Starus AI Content Creation Platform")
 
     # Create a session state to store the form visibility
     session_state = st.session_state
 
-    if not session_state.get("show_login_form"):
-        session_state.show_login_form = False
-
-    # Center-align the button using CSS
-    st.write("<style>div.row-widget.stButton > div{display:flex; justify-content:center;}</style>", unsafe_allow_html=True)
-
-    # Increase the button's size
-    st.write("<style>div.row-widget.stButton > button{width: auto; padding: 10px 30px;}</style>", unsafe_allow_html=True)
-
-    # Margin for the button
-    st.write("<style>div.row-widget.stButton > button{margin-top: 50px;}</style>", unsafe_allow_html=True)
+    # Load CSS styles
+    load_styles()
 
     # "Login" button
     if st.button("Login", key="login_button", help="Click to login"):
@@ -24,18 +16,22 @@ def main():
 
     if session_state.show_login_form:
         show_login_form()
+    else:
+        show_dashboard()
 
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-image: url("https://raw.githubusercontent.com/Starnez/StarusAI-ai-Platform/main/Background.png");
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+
+def load_styles():
+    # Center-align the button using CSS
+    st.write("<style>div.row-widget.stButton > div{display:flex; justify-content:center;}</style>",
+             unsafe_allow_html=True)
+
+    # Increase the button's size
+    st.write("<style>div.row-widget.stButton > button{width: auto; padding: 10px 30px;}</style>",
+             unsafe_allow_html=True)
+
+    # Margin for the button
+    st.write("<style>div.row-widget.stButton > button{margin-top: 50px;}</style>", unsafe_allow_html=True)
+
 
 def show_login_form():
     st.subheader("Login to your account")
@@ -50,6 +46,27 @@ def show_login_form():
             st.success("Logged in successfully!")
         else:
             st.warning("Please enter your username and password.")
+
+
+def show_dashboard():
+    # Sidebar
+    st.sidebar.title("Dashboard")
+
+    # Placeholder for profile picture
+    st.sidebar.image("placeholder_image.png", width=100)
+
+    # Placeholder for name and company
+    st.sidebar.write("John Doe")
+    st.sidebar.write("Company, Inc.")
+
+    # Dashboard dropdown menu
+    st.sidebar.write("Dashboard")
+    # Add other menu items here
+
+    # Main content
+    st.write("Welcome to the Dashboard!")
+    # Add content for the Dashboard page here
+
 
 if __name__ == "__main__":
     main()
